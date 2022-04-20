9167bcc6eaeecdb2249575ba712d2804ddc6c16f
webala <webala1001@gmail.com>
2022-04-20 08:56:15 +0300

    initial commit

---
+++ src/backend/contracts/NFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage {
+    uint256 public tokenCount; //will be implicitly set to 0
+
+    constructor() ERC721("WebbD", "WEBB") {}
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return tokenCount;
+    }
+}
