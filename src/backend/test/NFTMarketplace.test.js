e7234d5548a336c195441e9ba44f2bcf60273179
webala <webala1001@gmail.com>
2022-04-20 15:51:18 +0300

    succes test case for tracking newly created item comlete

--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -1,6 +1,9 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
+const toWei = num => ethers.utils.parseEther(num.toString())
+const fromWei = num => ethers.utils.formatEther(num)
+
 describe('NFTMarketplace', function() {
 
     let deployer, addr1, addr2, nft, marketplace
@@ -50,13 +53,34 @@ describe('NFTMarketplace', function() {
     describe('Making marketplace items', function () {
         beforeEach(async function () {
             //address 1 mints nft
-            awaitnft.connect(addr1),mint(URI)
+            awaitnft.connect(addr1).mint(URI)
             //address 1 approves marketplace to spend nft
             await nft.connect(addr1).setApprovalForAll(marketplace.address, true) 
         })
 
         it("Should track newly created item, transfer NFT from seller to marketplace and emit Offered event", async function () {
-            
+            // addr1 offers their nft at a price of 1 ether
+            await expect(marketplace.connect(addr1).makeItem(nft.address, 1, toWei(1)))
+            .to.emit(marketplace, 'Offered')
+            .withArgs(
+                1,
+                nft.address,
+                1,
+                toWei(1),
+                addr1.address
+            )
+            //Owner of nft should now be marketplace
+            expect(await nft.ownerOf(1)).to.equal(marketplace.address)
+            //Item count should now equal 1
+            expect(await marketplace.itemCount()).to.equal(1)
+            //Get item from items mapping and make sure the fields are correct
+            const item =await marketplace.items(1)
+            expect(item.itemId).to.equal(1)
+            expect(item.nft).to.equal(nft.address)
+            expect(item.tokenId).to.equal(1)
+            expect(item.price).to.equal(toWei(1))
+            expect(item.sold).to.equal(false)
+
         })
     })
 })
